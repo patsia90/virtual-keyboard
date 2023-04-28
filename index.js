b93cc9bb24d2f137de962d0bd84321ea9bdc198f
MalS <90wide@gmail.com>
2023-04-28 21:49:57 +0300

    feat: add animation for the virtul keyboard when pressing on the phusical one

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const textArea = document.createElement('textarea')
 textArea.classList.add('textarea')
 textArea.setAttribute('rows', '5')
 textArea.setAttribute('cols', '50')
-textArea.setAttribute('id', 'textarea')
 
 const keyboard = document.createElement('div')
 keyboard.classList.add('keyboard')
@@ -11,70 +10,70 @@ const wrapper = document.createElement('div')
 wrapper.classList.add('wrapper')
 
 const keys = [
-  { key: '`', shift: '~', keyCode: 192 },
-  { key: '1', shift: '!', keyCode: 49 },
-  { key: '2', shift: '@', keyCode: 50 },
-  { key: '3', shift: '#', keyCode: 51 },
-  { key: '4', shift: '$', keyCode: 52 },
-  { key: '5', shift: '%', keyCode: 53 },
-  { key: '6', shift: '^', keyCode: 54 },
-  { key: '7', shift: '&', keyCode: 55 },
-  { key: '8', shift: '*', keyCode: 56 },
-  { key: '9', shift: '(', keyCode: 57 },
-  { key: '0', shift: ')', keyCode: 48 },
-  { key: '-', shift: '_', keyCode: 189 },
-  { key: '=', shift: '+', keyCode: 187 },
-  { key: 'Backspace', keyCode: 8, class: 'backspace' },
-  { key: 'Tab', keyCode: 9, class: 'tab' },
-  { key: 'q', shift: 'Q', keyCode: 81 },
-  { key: 'w', shift: 'W', keyCode: 87 },
-  { key: 'e', shift: 'E', keyCode: 69 },
-  { key: 'r', shift: 'R', keyCode: 82 },
-  { key: 't', shift: 'T', keyCode: 84 },
-  { key: 'y', shift: 'Y', keyCode: 89 },
-  { key: 'u', shift: 'U', keyCode: 85 },
-  { key: 'i', shift: 'I', keyCode: 73 },
-  { key: 'o', shift: 'O', keyCode: 79 },
-  { key: 'p', shift: 'P', keyCode: 80 },
-  { key: '[', shift: '{', keyCode: 219 },
-  { key: ']', shift: '}', keyCode: 221 },
-  { key: '\\', shift: '|', keyCode: 220 },
-  { key: 'Del', keyCode: 46, class: 'del' },
-  { key: 'CapsLock', keyCode: 20, class: 'capslock' },
-  { key: 'a', shift: 'A', keyCode: 65 },
-  { key: 's', shift: 'S', keyCode: 83 },
-  { key: 'd', shift: 'D', keyCode: 68 },
-  { key: 'f', shift: 'F', keyCode: 70 },
-  { key: 'g', shift: 'G', keyCode: 71 },
-  { key: 'h', shift: 'H', keyCode: 72 },
-  { key: 'j', shift: 'J', keyCode: 74 },
-  { key: 'k', shift: 'K', keyCode: 75 },
-  { key: 'l', shift: 'L', keyCode: 76 },
-  { key: ';', shift: ':', keyCode: 186 },
-  { key: "'", shift: '"', keyCode: 222 },
-  { key: 'Enter', keyCode: 13, class: 'enter' },
-  { key: 'Shift', keyCode: 16, location: 'left', class: 'shift-left' },
-  { key: 'z', shift: 'Z', keyCode: 90 },
-  { key: 'x', shift: 'X', keyCode: 88 },
-  { key: 'c', shift: 'C', keyCode: 67 },
-  { key: 'v', shift: 'V', keyCode: 86 },
-  { key: 'b', shift: 'B', keyCode: 66 },
-  { key: 'n', shift: 'N', keyCode: 78 },
-  { key: 'm', shift: 'M', keyCode: 77 },
-  { key: ',', shift: '<', keyCode: 188 },
-  { key: '.', shift: '>', keyCode: 190 },
-  { key: '/', shift: '?', keyCode: 191 },
-  { key: '▲', keyCode: 38, class: 'arrow-up' },
-  { key: 'Shift', keyCode: 16, location: 'rigt', class: 'shift-right' },
-  { key: 'Ctrl', keyCode: 17, location: 'left', class: 'ctrl' },
-  { key: 'Win', keyCode: '', class: 'win' },
-  { key: 'Alt', keyCode: 18, location: 'left', class: 'alt' },
-  { key: 'Space', keyCode: 32, class: 'space' },
-  { key: 'Alt', keyCode: 18, location: 'rigt', class: 'alt' },
-  { key: '◄', keyCode: 37, class: 'arrow-left' },
-  { key: '▼', keyCode: 40, class: 'arrow-down' },
-  { key: '►', keyCode: 39, class: 'arrow-right' },
-  { key: 'Ctrl', keyCode: 17, location: 'rigt', class: 'ctrl' },
+  { key: '`', shift: '~', code: 'Backquote' },
+  { key: '1', shift: '!', code: 'Digit1' },
+  { key: '2', shift: '@', code: 'Digit2' },
+  { key: '3', shift: '#', code: 'Digit3' },
+  { key: '4', shift: '$', code: 'Digit4' },
+  { key: '5', shift: '%', code: 'Digit5' },
+  { key: '6', shift: '^', code: 'Digit6' },
+  { key: '7', shift: '&', code: 'Digit7' },
+  { key: '8', shift: '*', code: 'Digit8' },
+  { key: '9', shift: '(', code: 'Digit9' },
+  { key: '0', shift: ')', code: 'Digit0' },
+  { key: '-', shift: '_', code: 'Minus' },
+  { key: '=', shift: '+', code: 'Equal' },
+  { key: 'Backspace', code: 'Backspace', class: 'backspace' },
+  { key: 'Tab', code: 'Tab', class: 'tab' },
+  { key: 'q', shift: 'Q', code: 'KeyQ' },
+  { key: 'w', shift: 'W', code: 'KeyW' },
+  { key: 'e', shift: 'E', code: 'KeyE' },
+  { key: 'r', shift: 'R', code: 'KeyR' },
+  { key: 't', shift: 'T', code: 'KeyT' },
+  { key: 'y', shift: 'Y', code: 'KeyY' },
+  { key: 'u', shift: 'U', code: 'KeyU' },
+  { key: 'i', shift: 'I', code: 'KeyI' },
+  { key: 'o', shift: 'O', code: 'KeyO' },
+  { key: 'p', shift: 'P', code: 'KeyP' },
+  { key: '[', shift: '{', code: 'BracketLeft' },
+  { key: ']', shift: '}', code: 'BracketRight' },
+  { key: '\\', shift: '|', code: 'Backslash' },
+  { key: 'Del', code: 'Delete', class: 'del' },
+  { key: 'CapsLock', code: 'CapsLock', class: 'capslock' },
+  { key: 'a', shift: 'A', code: 'KeyA' },
+  { key: 's', shift: 'S', code: 'KeyS' },
+  { key: 'd', shift: 'D', code: 'KeyD' },
+  { key: 'f', shift: 'F', code: 'KeyF' },
+  { key: 'g', shift: 'G', code: 'KeyG' },
+  { key: 'h', shift: 'H', code: 'KeyH' },
+  { key: 'j', shift: 'J', code: 'KeyJ' },
+  { key: 'k', shift: 'K', code: 'KeyK' },
+  { key: 'l', shift: 'L', code: 'KeyL' },
+  { key: ';', shift: ':', code: 'Semicolon' },
+  { key: "'", shift: '"', code: 'Quote' },
+  { key: 'Enter', code: 'Enter', class: 'enter' },
+  { key: 'Shift', code: 'ShiftLeft', location: 1, class: 'shift-left' },
+  { key: 'z', shift: 'Z', code: 'KeyZ' },
+  { key: 'x', shift: 'X', code: 'KeyX' },
+  { key: 'c', shift: 'C', code: 'KeyC' },
+  { key: 'v', shift: 'V', code: 'KeyV' },
+  { key: 'b', shift: 'B', code: 'KeyB' },
+  { key: 'n', shift: 'N', code: 'KeyN' },
+  { key: 'm', shift: 'M', code: 'KeyM' },
+  { key: ',', shift: '<', code: 'Comma' },
+  { key: '.', shift: '>', code: 'Period' },
+  { key: '/', shift: '?', code: 'Slash' },
+  { key: '▲', code: 'ArrowUp', class: 'arrow-up' },
+  { key: 'Shift', code: 'ShiftRight', location: 2, class: 'shift-right' },
+  { key: 'Ctrl', code: 'ControlLeft', class: 'ctrl' },
+  { key: 'Win', code: 'MetaLeft', class: 'win' },
+  { key: 'Alt', code: 'AltLeft', location: 1, class: 'alt' },
+  { key: 'Space', code: 'Space', class: 'space' },
+  { key: 'Alt', code: 'AltRight', location: 2, class: 'alt' },
+  { key: '◄', code: 'ArrowLeft', class: 'arrow-left' },
+  { key: '▼', code: 'ArrowDown', class: 'arrow-down' },
+  { key: '►', code: 'ArrowRight', class: 'arrow-right' },
+  { key: 'Ctrl', code: 'ControlRight', location: 'rigt', class: 'ctrl' },
 ]
 
 // Create a function to create buttons on the keyboard
@@ -86,7 +85,7 @@ function createKeys() {
     const button = document.createElement('button')
     button.classList.add('key', `${key.class}`)
     button.textContent = key.key
-    button.dataset.keycode = key.keyCode
+    button.dataset.code = key.code
     if (key.shift) {
       button.dataset.shift = key.shift
     }
@@ -97,10 +96,10 @@ function createKeys() {
 
 // Create a function to handle pressing buttons
 function handleKeys(e) {
-  const key = e.target.dataset.keycode
+  const key = e.target.dataset.code
   const shift = e.shiftKey
   if (key) {
-    const event = new KeyboardEvent('keydown', { keyCode: shift ? e.target.dataset.shift.charCodeAt() : key })
+    const event = new KeyboardEvent('keydown', { code: shift ? e.target.dataset.shift.charCodeAt() : key })
     document.dispatchEvent(event)
   }
 }
@@ -114,10 +113,56 @@ document.body.appendChild(keyboard)
 const keysContainer = document.querySelector('.keyboard__container')
 keysContainer.addEventListener('click', handleKeys)
 
-// Add cymbos by keyboard
-document.addEventListener('keydown', function (event) {
-  if (event.key) {
-    console.log(event.key)
+// Add symbos by keyboard
+document.addEventListener('keydown', printKeysByKeyboard)
+
+function printKeysByKeyboard(event) {
+  if (
+    !event.key ===
+    ['ArrowLeft', 'ArrowRight', 'Delete', 'Backspace', 'Enter', 'Shift', 'Alt', 'Control', 'CapsLock', 'Tab'].includes(
+      event.key
+    )
+  ) {
     textArea.value += event.key
   }
+}
+
+// Disable standard input event handling
+textArea.addEventListener('keydown', function (event) {
+  if (event.type === 'keydown') {
+    event.preventDefault()
+  }
 })
+
+// Add handle for the backspace
+document.addEventListener('keydown', dellBackSpace)
+
+function dellBackSpace(event) {
+  if (event.key === 'Backspace') {
+    textArea.value = textArea.value.slice(0, -1)
+  }
+}
+
+// Add handle for the Enter
+document.addEventListener('keydown', lineBreak)
+
+function lineBreak(event) {
+  if (event.key === 'Enter') {
+    textArea.value += '\n'
+  }
+}
+
+// Add animation for the virtual keyboard when pressing on the physical one
+document.addEventListener('keydown', showButton)
+
+function showButton(event) {
+  const keyBoard = document.querySelectorAll('.keyboard__container .key')
+  keyBoard.forEach(function (element) {
+    if (element.getAttribute('data-code') === event.code) {
+      element.classList.add('active')
+    }
+    setTimeout(() => {
+      element.classList.remove('active')
+    }, 200)
+  })
+}
